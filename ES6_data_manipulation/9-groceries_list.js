cf0326c70d84c62f641e652111994bcebd12774a
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-05-26 22:50:25 -0400

    map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const shopList = new Map();
+  shopList.set('Apples', 10);
+  shopList.set('Tomatoes', 10);
+  shopList.set('Pasta', 1);
+  shopList.set('Rice', 1);
+  shopList.set('Banana', 5);
+  return shopList;
+}
